9ef9f77e7f23f210dea99f456ddf459f4584d5a8
drry <drry@drry.jp>
2008-11-11 12:44:08 +0000

    * added a dummy PI to the test.  * et cetera.

--- lib/XML/Liberal/Remedy/DeprecatedDTD.pm
+++ lib/XML/Liberal/Remedy/DeprecatedDTD.pm
@@ -5,8 +5,12 @@ use base qw( XML::Liberal::Remedy );
 sub apply {
     my $self = shift;
     my($xml_ref) = @_;
-    $$xml_ref =~ s{(?<=\s(["'])http://)my\.netscape\.com/publish/formats(?=/rss-0\.91?\.dtd\1\s*>)}
-                  {www.rssboard.org};
+    my $match = $$xml_ref =~ s{(?<=\s(["'])http://)my\.netscape\.com/publish/formats(?=/rss-0\.91?\.dtd\1\s*>)}
+                              {www.rssboard.org};
+    return 1 if $match;
+
+    Carp::carp("Can't find deprecated DTD: line $self->{line}");
+    return;
 }
 
 1;
