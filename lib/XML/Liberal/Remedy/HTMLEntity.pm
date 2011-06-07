086e3160eb0c3fa99ee06d7cb9fb3cfe90d6cb9b
Aaron Crane <arc@cpan.org>
2011-06-07 16:32:59 +0100

    Better error message when HTMLEntity remedy fails
    Previously emitted the somewhat obscure message "can't find named HTML
    entities", when the remedy found no named entity references that it was able
    to convert to a numeric character reference.  Now emits an entity-specific
    message for the first such unrecognised entity reference.
    
    This involves changing our dependency from HTML::Entities::Numbered to plain
    old HTML::Entities.

--- lib/XML/Liberal/Remedy/HTMLEntity.pm
+++ lib/XML/Liberal/Remedy/HTMLEntity.pm
@@ -2,20 +2,24 @@ package XML::Liberal::Remedy::HTMLEntity;
 use strict;
 use base qw( XML::Liberal::Remedy );
 
-use HTML::Entities::Numbered;
+use HTML::Entities ();
+
+my %DECODE = map {
+    (my $name = $_) =~ s{\;\z}{};
+    $name => sprintf '&#x%x;', ord $HTML::Entities::entity2char{$_}
+} keys %HTML::Entities::entity2char;
 
 # optimized to fix all errors in one apply() call
 sub apply {
     my $self = shift;
     my($xml_ref) = @_;
 
-    my $old = $$xml_ref;
-    $$xml_ref = name2hex_xml($$xml_ref);
+    $$xml_ref =~ s{&([a-zA-Z0-9]+);}{
+        $DECODE{$1}
+            || Carp::carp("Can't find named HTML entity $1, line $self->{line} pos $self->{pos}: $self->{error}")
+    }ge;
 
-    return 1 if $$xml_ref ne $old;
-
-    Carp::carp("Can't find named HTML entities, line $self->{line} pos $self->{pos}: $self->{error}");
-    return;
+    return 1;
 }
 
 1;
