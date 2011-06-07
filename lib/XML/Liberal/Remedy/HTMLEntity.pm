4c273425dcc9cfae663a7b4d28fa33b1480a9fa4
Aaron Crane <arc@cpan.org>
2011-06-07 16:40:20 +0100

    Ignore case in HTMLEntity remedy
    I have seen real-world feeds containing such things; this one sometimes
    does, for example: http://www.prospect.org/articles_rss.jsp

--- lib/XML/Liberal/Remedy/HTMLEntity.pm
+++ lib/XML/Liberal/Remedy/HTMLEntity.pm
@@ -14,8 +14,13 @@ sub apply {
     my $self = shift;
     my($xml_ref) = @_;
 
+    # Note that we can't tell whether "&EACUTE;" is meant to be "&eacute;"
+    # or "&Eacute;", so we arbitrarily choose "&eacute;".  Fortunately, the
+    # only HTML entities whose names aren't all-lower-case are the
+    # upper-case equivalents of all-lower-case ones, so this doesn't
+    # introduce any ambiguity that didn't exist in the source document.
     $$xml_ref =~ s{&([a-zA-Z0-9]+);}{
-        $DECODE{$1}
+        $DECODE{$1} || $DECODE{lc $1}
             || Carp::carp("Can't find named HTML entity $1, line $self->{line} pos $self->{pos}: $self->{error}")
     }ge;
 
