ada2c3cfc84f0874f8a614a43ff8e22ee91f207d
Aaron Crane <arc@cpan.org>
2011-06-08 16:55:41 +0100

    Make LowAsciiChars remedy slightly laxer
    Now allows any number of leading zeroes on the numeric character reference.

--- lib/XML/Liberal/Remedy/LowAsciiChars.pm
+++ lib/XML/Liberal/Remedy/LowAsciiChars.pm
@@ -2,32 +2,31 @@ package XML::Liberal::Remedy::LowAsciiChars;
 use strict;
 use base qw( XML::Liberal::Remedy );
 
+my @low_ascii = (0..8, 11..12, 14..31, 127);
+my $dec_rx = do {
+    my $pat = join '|', @low_ascii;
+    qr/$pat/;
+};
+my $hex_rx = do {
+    my $pat = join '|', map { sprintf '%x', $_ } @low_ascii;
+    qr/$pat/i;
+};
+
 sub handles_error {
     my $class = shift;
     my($error, $error1, $error2) = @_;
 
-    return $error =~ /^:\d+: parser error : xmlParseCharRef: invalid xmlChar value \d+/;
+    return $error =~ /^:\d+: parser error : xmlParseCharRef: invalid xmlChar value $dec_rx\b/;
 }
 
 # optimized to fix all errors in one apply() call
 sub apply {
     my $self = shift;
     my($xml_ref) = @_;
-    my $match = $$xml_ref =~ s{(&#(?:(\d+)|x([0-9A-Fa-f]{2,4}));)}{
-        ($2 && is_low_ascii($2)) || ($3 && is_low_ascii(hex($3)))
-            ? '' : $1;
-    }eg;
-    return 1 if $match;
+    return 1 if $$xml_ref =~ s{&#(?:0*$dec_rx|[xX]0*$hex_rx);}{}g;
 
     Carp::carp("Can't find low ascii bytes $self->{error}");
     return;
-}
-
-my %is_low_ascii = map { $_ => 1 } (0..8, 11..12, 14..31, 127);
-
-sub is_low_ascii {
-    my $num = shift;
-    $is_low_ascii{$num};
 }
 
 1;
