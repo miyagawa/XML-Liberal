d252da2729a15d266038a07dc1d732d82c05c2b0
Aaron Crane <arc@cpan.org>
2011-06-11 17:09:41 +0100

    Make EntityRef remedy smarter about CDATA sections
    If the document contains any CDATA sections (or PIs), it fixes
    broken-ampersand errors one at a time, as discovered by the parser.  This
    preserves the expected content of those CDATA sections and PIs.
    
    If the document doesn't contain any, we bulk-fix all ampersands for
    efficiency, just as before.

--- lib/XML/Liberal/Remedy/EntityRef.pm
+++ lib/XML/Liberal/Remedy/EntityRef.pm
@@ -1,7 +1,6 @@
 package XML::Liberal::Remedy::EntityRef;
 use strict;
 
-# optimized to fix all errors in one apply() call
 sub apply {
     my $class = shift;
     my($driver, $error, $xml_ref) = @_;
@@ -9,8 +8,27 @@ sub apply {
     return 0 if $error->message !~
         /^parser error : (?:EntityRef: expecting ';'|xmlParseEntityRef: no name)/;
 
-    return 1 if $$xml_ref =~
-        s/&(?!\w+;|#(?:x[a-fA-F0-9]+|\d+);)/&amp;/g;
+    # If the document doesn't contain any PIs or CDATA sections, we might as
+    # well try to fix all broken entity references and named character
+    # references in one go.  (If it does contain one of those, fixing
+    # everything would risk changing the content in ways the author wouldn't
+    # expect.)
+    #
+    # In principle, we should take care not to change comments either; but
+    # in practice, I'm prepared to consider comments fair game, given that
+    # the author can preserve them by merely generating a well-formed XML
+    # document.
+    if ($$xml_ref !~ / .<\? | <!\[CDATA\[/xms) {
+        return 1 if $$xml_ref =~
+            s/&(?!\w+;|#(?:x[a-fA-F0-9]+|\d+);)/&amp;/g;
+    }
+    else {
+        my $pos = $error->location($xml_ref);
+        while ($pos > 0) {
+            pos($$xml_ref) = $pos--;
+            return 1 if $$xml_ref =~ s/\G&/&amp;/
+        }
+    }
 
     Carp::carp("Can't find unescaped &, error was: ", $error->summary);
     return 0;
